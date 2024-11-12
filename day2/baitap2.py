498ad060b3491cfeecbf8e8a2414a7fb114c7830
TUDONG05 <khactu731@gmail.com>
2024-11-12 21:26:05 +0700

    Add files via upload

---
+++ day2/baitap2.py
@@ -0,0 +1,11 @@
+score= float (input("nhap diem tong ket mon tieng anh:  "))
+if score > 8.5:
+    print("xep loai A")
+elif score >= 7.00:
+    print("xep loai B")
+elif score >=5.00:
+    print("xep loai C")
+elif score >=4.00:
+    print("xep loai D")
+else:
+    print("Ban da danh mat 3.112.50011!")
