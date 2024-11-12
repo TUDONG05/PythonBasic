aff2a0c0b97aa3f2467394af5a932a1190ab9007
TUDONG05 <khactu731@gmail.com>
2024-11-12 21:16:33 +0700

    Add files via upload

---
+++ day2/bai1.py
@@ -0,0 +1,9 @@
+
+n = int(input("nhap so ban trong lop Lab 601: "))
+m = int(input("nhap so hop qua thay co: "))
+k = int(input("nhap so tui qua trong tung hop:"))
+if (m * k ) % n == 0:
+    print("chia deu duoc cho cac ban!")
+else:
+    print("khong chia deu duoc cho cac ban!")
+
